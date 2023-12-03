eb6716bdda6f558297c2479b888ae4f41428ea40
João Pedro Miranda Salim <joaopedromirandasalim@gmail.com>
2023-12-03 19:51:47 -0300

    Variáveis recebem elementos do html do inimigo

--- main.js
+++ main.js
@@ -3,3 +3,8 @@
 const tabuleiroJogador = document.querySelectorAll("#jogador .caixa img");
 const somaTextoJogador = document.querySelectorAll("#somaPlayer .caixa p");
 const caixasJogador = document.querySelectorAll("#jogador .caixa");
+
+//Recebe os elementos do HTML do inimigo
+const tabuleiroInimigo = document.querySelectorAll("#inimigo .caixa img");
+const somaTextoInimigo = document.querySelectorAll("#somaInimigo .caixa p");
+const caixasInimigo = document.querySelectorAll("#inimigo .caixa");
