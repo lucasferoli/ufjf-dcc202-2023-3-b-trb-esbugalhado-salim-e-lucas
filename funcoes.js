cd0954bb9b1fc824bb450b5e3bf61c7cb168f045
João Pedro Miranda Salim <joaopedromirandasalim@gmail.com>
2023-12-03 19:46:21 -0300

    Importa sprites e cria função para retornar o sprite

--- funcoes.js
+++ funcoes.js
@@ -1,3 +1,16 @@
+const spritesDados = [
+    'Sprites/dado1.png',
+    'Sprites/dado2.png',
+    'Sprites/dado3.png',
+    'Sprites/dado4.png',
+    'Sprites/dado5.png',
+    'Sprites/dado6.png'
+]
+
 function rolarDado(){
     return Math.floor(Math.random() * 6) + 1;
+}
+
+function imagemDado(indice){
+    return spritesDados[indice];
 }
