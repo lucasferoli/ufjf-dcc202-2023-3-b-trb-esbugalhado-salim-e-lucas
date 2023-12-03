525ffea62c1271768b85eda97283f9ca9aece90f
João Pedro Miranda Salim <joaopedromirandasalim@gmail.com>
2023-12-03 19:49:35 -0300

    Variáveis recebem elementos do html do jogador

---
+++ main.js
@@ -0,0 +1,5 @@
+
+//Recebe os elementos do HTML do jogador
+const tabuleiroJogador = document.querySelectorAll("#jogador .caixa img");
+const somaTextoJogador = document.querySelectorAll("#somaPlayer .caixa p");
+const caixasJogador = document.querySelectorAll("#jogador .caixa");
