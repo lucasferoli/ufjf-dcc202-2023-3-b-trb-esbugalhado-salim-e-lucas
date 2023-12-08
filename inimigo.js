72b29c5969663765522ae1661dba6936fdb5327d
João Pedro Miranda Salim <joaopedromirandasalim@gmail.com>
2023-12-08 20:11:35 -0300

    Cria função selecionaColunaVazia

--- inimigo.js
+++ inimigo.js
@@ -1,7 +1,7 @@
 //Funções sobre as jogadas do inimigo
 function colunaValida(coluna){
     for(let i = 0; i < 3; i++){
-        if(coluna[i] == 0){
+        if(coluna[i] === 0){
             return true;
         }
     }
@@ -13,12 +13,12 @@ function colunaMal(colunaInimigo, colunaJogador, dadoInimigo){
     let qtd = [0, 0, 0];
     for(let i = 0; i < 3; i++){
         for(let j = 0; j < 3; j++){
-            if(colunaJogador[i][j] == dadoInimigo){
+            if(colunaJogador[i][j] === dadoInimigo){
                 qtd[i]++;
             }
         }
     }
-    
+
     //Se não há jogada para prejudicar o jogador, faz aleatória
     if(qtd[0] === 0 && qtd[1] === 0 && qtd[2] === 0){
         let coluna = Math.floor(Math.random() * 3);
@@ -39,6 +39,42 @@ function colunaMal(colunaInimigo, colunaJogador, dadoInimigo){
     return 0;
 }
 
+
+
+//FIXME: Não está funcionando corretamente
+function colunaBom(colunaInimigo, dadoInimigo){
+    let qtd = [0, 0, 0];
+    for(let i = 0; i < 3; i++){
+        for(let j = 0; j < 3; j++){
+            if(colunaInimigo[i][j] === dadoInimigo){
+                qtd[i]++;
+            }
+        }
+    }
+    
+    let maior = ordenaIndices(qtd);
+    
+    if(qtd[0] == 0 && qtd[1] == 0 && qtd[2] == 0){
+        
+    }
+
+    for(let i = 0; i < 3; i++){
+        if(colunaInimigo[maior[i]][2] == 0){
+            return maior[i];
+        }
+    }
+    return 0;
+}
+
+function selecionaColunaVazia(colunaInimigo){
+    let maior = ordenaIndices(colunaInimigo);
+    for(let i = 3; i >= 0; i--){
+        if(!colunaValida(colunaInimigo[maior[i]])){
+            return maior[i];
+        }
+    }
+    
+}
 //Função que ordena os índices de um vetor de forma decrescente
 function ordenaIndices(coluna){
     let maior = [0, 1, 2];
@@ -55,35 +91,6 @@ function ordenaIndices(coluna){
         }
     }
     return maior;
-}
-
-
-//FIXME: Não está funcionando corretamente
-function colunaBom(colunaInimigo, dadoInimigo){
-    let qtd = [0, 0, 0];
-    for(let i = 0; i < 3; i++){
-        for(let j = 0; j < 3; j++){
-            if(colunaInimigo[i][j] == dadoInimigo){
-                qtd[i]++;
-            }
-        }
-    }
-    if(qtd[0] == 0 && qtd[1] == 0 && qtd[2] == 0){
-        let coluna = Math.floor(Math.random() * 3);
-        while(colunaInimigo[coluna][2] != 0){
-            coluna = Math.floor(Math.random() * 3);
-        }
-        return coluna;
-    }
-
-    let maior = ordenaIndices(qtd);
-
-    for(let i = 0; i < 3; i++){
-        if(colunaInimigo[maior[i]][2] == 0){
-            return maior[i];
-        }
-    }
-    return 0;
 }
 
 export {colunaBom, colunaMal};
