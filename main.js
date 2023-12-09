47729fc85318a321b2758f4514c2d70d84f02199
João Pedro Miranda Salim <joaopedromirandasalim@gmail.com>
2023-12-08 23:17:53 -0300

    Altera para o inimigo inicial ser aleatório

--- main.js
+++ main.js
@@ -68,7 +68,7 @@ setVencedorTexto(vencedorTexto);
 const reiniciarBotao = document.querySelector("button.reinicia");
 setReiniciarBotao(reiniciarBotao);
 
-setInimigo(0);
+setInimigo(Math.floor(Math.random() * 3));
 //Para cada botão, passa a função com o parâmetro da coluna
 for(let i = 0; i < 3; i++){
     botoes[i].addEventListener("click", () => acaoJogadorAqui(i));
